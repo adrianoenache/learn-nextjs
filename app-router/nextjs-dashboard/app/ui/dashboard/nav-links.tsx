3712bdf480c221e9684a6148ab4d8d377bc4a805
Adriano Enache <adriano.enache@publicisgroupe.com>
2025-11-02 14:33:57 -0300

    feat: refactor NavLinks component to use Next.js Link for client-side navigation

--- app-router/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ app-router/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,19 @@
+// Client component
+'use client';
+
 // Import icons
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
+// Import Next.js components
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// Import clsx for conditional class names
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -28,6 +38,9 @@ const links = [
 // Navigation links component
 export default function NavLinks() {
 
+  // Get the current pathname
+  const pathname = usePathname();
+
   // Return the list of navigation links
   return (
 
@@ -43,16 +56,20 @@ export default function NavLinks() {
         // Return individual link
         return (
 
-          // Link element
-          <a
+          // Link component from Next.js for client-side navigation
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
-            {/* Icon container */}
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
+            <span className="hidden md:block">{link.name}</span>
+          </Link>
 
         );
 
